009aba3000fd05840fadef659c594b2f5247e4ce
Shivam Prakash <65006707+shivam12prakash@users.noreply.github.com>
2021-01-07 15:25:40 +0530

    Update users.js

--- src/utils/users.js
+++ src/utils/users.js
@@ -2,16 +2,18 @@ const users = []
 
 
 const addUser = ({ id, username, room }) => {
-    // Clean the data
-    username = username.trim().toLowerCase()
-    room = room.trim().toLowerCase()
-
+   
     // Validate the data
     if (!username || !room) {
         return {
             error: 'Username and room are required!'
         }
     }
+    
+     // Clean the data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
 
     // Check for existing user
     const existingUser = users.find((user) => {
